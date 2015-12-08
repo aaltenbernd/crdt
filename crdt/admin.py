a07394358825d7e2c351c0466c37819e60aaa137
Anton Altenbernd <anton.altenbernd@campus.tu-berlin.de>
2015-12-08 13:31:47 +0100

    admin page : user profile, incoming operation, outgoing operation

--- crdt/admin.py
+++ crdt/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, IncomingOperation, OutgoingOperation
 
-# do in shell
+admin.site.register(UserProfile)
+admin.site.register(IncomingOperation)
+admin.site.register(OutgoingOperation)
