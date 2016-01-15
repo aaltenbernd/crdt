046c6cb3b04ab2bd59fc5d3326d7361910bc7626
aaltenbernd <anton.altenbernd@campus.tu-berlin.de>
2016-01-15 12:22:53 +0100

    using content of develop branch

--- crdt/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Number, Node, IncomingOperation, OutgoingOperation
-
-admin.site.register(Number)
-admin.site.register(Node)
-admin.site.register(IncomingOperation)
-admin.site.register(OutgoingOperation)
