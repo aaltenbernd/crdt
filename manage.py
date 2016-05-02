31318d69d802802a0e875b17a4010745137ee162
aaltenbernd <anton.altenbernd@campus.tu-berlin.de>
2016-05-02 16:21:43 +0200

    final commit bachelor thesis deadline 05 03 2016

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import django
 
 if __name__ == "__main__":
 
+	# starting local host id = 0
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.host_0")
 
 	from django.core.management import execute_from_command_line
